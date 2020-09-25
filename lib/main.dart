1241c9c186b92f7983d254e23ee16d1d6d6689d2
n-summer-0808 <34152847+n-summer-0808@users.noreply.github.com>
2020-09-26 00:27:28 +0900

    コメント追加, json処理書きかけ

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart' show rootBundle;
+import 'dart:math';
+import 'dart:convert';
+import 'package:charts_flutter/flutter.dart' as charts;
 
 void main() {
-  runApp(App());
+  // アプリ起動時にmainが呼び出される
+  runApp(App()); // Appを呼ぶ
 }
 
 class App extends StatelessWidget {
+  // mainから呼ばれる
   @override
   Widget build(BuildContext context) {
     final title = 'poi-view';
     return MaterialApp(
-      theme: ThemeData(
-          brightness: Brightness.light, primaryColor: Colors.blueGrey),
       home: Scaffold(
         appBar: AppBar(
-          title: Text(title),
+          title: Text(title), // アプリ最上部のタイトル表示
         ),
         body: Container(
           child: Column(
+            // 複数のwidgetを縦に配置
             children: <Widget>[
               Container(
-                height: 100, // 縦幅
-                child: ChangeForm(),
+                height: 100, // 高さを指定
+                child: ChangeForm(), // エリア指定フォームを配置
               ),
-              PoiView(),
+              PoiView(), // グラフ表示を配置
             ],
           ),
         ),
@@ -32,6 +37,7 @@ class App extends StatelessWidget {
 }
 
 class PoiView extends StatefulWidget {
+  // グラフ表示
   @override
   _PoiViewState createState() => _PoiViewState();
 }
@@ -44,10 +50,11 @@ class _PoiViewState extends State<PoiView> {
         crossAxisCount: 2,
         children: List.generate(50, (index) {
           return Center(
-            child: Text(
-              'Item $index',
-              style: Theme.of(context).textTheme.headline5,
-            ),
+            child: MyHomePage(),
+            // child: Text(
+            //   'Item $index',
+            //   style: Theme.of(context).textTheme.headline5,
+            // ),
           );
         }),
       ),
@@ -61,6 +68,7 @@ class ChangeForm extends StatefulWidget {
 }
 
 class _ChangeFormState extends State<ChangeForm> {
+  // エリア指定フォーム
   String _defaultValue = '嵐山エリア';
   List<String> _list = <String>['嵐山エリア', '祇園エリア', '奈良エリア'];
   String _text = '';
@@ -86,4 +94,120 @@ class _ChangeFormState extends State<ChangeForm> {
       ),
     );
   }
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String _data = "Load JSON Data";
+
+  void _updateJsonData() {
+    setState(() {
+      loadJsonAsset();
+    });
+  }
+
+  Future<void> loadJsonAsset() async {
+    _data = "";
+    String loadData = await rootBundle.loadString('json/data.json');
+    final jsonResponse = json.decode(loadData);
+    jsonResponse.forEach((key, value) => _data = _data + '$key: $value \x0A');
+  }
+}
+
+class ExportGraph extends StatefulWidget {
+  @override
+  _ExportGraphState createState() => _ExportGraphState();
+}
+
+class _ExportGraphState extends State<ExportGraph> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+class SimpleTimeSeriesChart extends StatelessWidget {
+  final List<charts.Series> seriesList;
+  final bool animate;
+
+  SimpleTimeSeriesChart(this.seriesList, {this.animate});
+
+  /// Creates a [TimeSeriesChart] with sample data and no transition.
+  factory SimpleTimeSeriesChart.withSampleData() {
+    return new SimpleTimeSeriesChart(
+      _createSampleData(),
+      // Disable animations for image tests.
+      animate: false,
+    );
+  }
+
+  // EXCLUDE_FROM_GALLERY_DOCS_START
+  // This section is excluded from being copied to the gallery.
+  // It is used for creating random series data to demonstrate animation in
+  // the example app only.
+  factory SimpleTimeSeriesChart.withRandomData() {
+    return new SimpleTimeSeriesChart(_createRandomData());
+  }
+
+  /// Create random data.
+  static List<charts.Series<TimeSeriesSales, DateTime>> _createRandomData() {
+    final random = new Random();
+
+    final data = [
+      new TimeSeriesSales(new DateTime(2017, 9, 19), random.nextInt(100)),
+      new TimeSeriesSales(new DateTime(2017, 9, 26), random.nextInt(100)),
+      new TimeSeriesSales(new DateTime(2017, 10, 3), random.nextInt(100)),
+      new TimeSeriesSales(new DateTime(2017, 10, 10), random.nextInt(100)),
+    ];
+
+    return [
+      new charts.Series<TimeSeriesSales, DateTime>(
+        id: 'Sales',
+        colorFn: (_, __) => charts.MaterialPalette.blue.shadeDefault,
+        domainFn: (TimeSeriesSales sales, _) => sales.time,
+        measureFn: (TimeSeriesSales sales, _) => sales.sales,
+        data: data,
+      )
+    ];
+  }
+  // EXCLUDE_FROM_GALLERY_DOCS_END
+
+  @override
+  Widget build(BuildContext context) {
+    return new charts.TimeSeriesChart(
+      seriesList,
+      animate: animate,
+      // Optionally pass in a [DateTimeFactory] used by the chart. The factory
+      // should create the same type of [DateTime] as the data provided. If none
+      // specified, the default creates local date time.
+      dateTimeFactory: const charts.LocalDateTimeFactory(),
+    );
+  }
+
+  /// Create one series with sample hard coded data.
+  static List<charts.Series<TimeSeriesSales, DateTime>> _createSampleData() {
+    final data = [
+      new TimeSeriesSales(new DateTime(2017, 9, 19), 5),
+      new TimeSeriesSales(new DateTime(2017, 9, 26), 25),
+      new TimeSeriesSales(new DateTime(2017, 10, 3), 100),
+      new TimeSeriesSales(new DateTime(2017, 10, 10), 75),
+    ];
+
+    return [
+      new charts.Series<TimeSeriesSales, DateTime>(
+        id: 'Sales',
+        colorFn: (_, __) => charts.MaterialPalette.blue.shadeDefault,
+        domainFn: (TimeSeriesSales sales, _) => sales.time,
+        measureFn: (TimeSeriesSales sales, _) => sales.sales,
+        data: data,
+      )
+    ];
+  }
+}
+
+/// Sample time series data type.
+class TimeSeriesSales {
+  final DateTime time;
+  final int sales;
+
+  TimeSeriesSales(this.time, this.sales);
 }
